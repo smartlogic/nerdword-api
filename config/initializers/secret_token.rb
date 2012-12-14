9416c97d4cd483c12c0371184b1289a19b4a5cb8
Eric Oestrich <eric@oestrich.org>
2012-12-14 11:16:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nerdword::Application.config.secret_token = '7ad609fd4c3c9ed4cf3697475d7ee67d950e149eb80df8280dbc24008fc2f39362aee2ae61d373c82115bf372f34aa0c0b52a9e2e340c02e19c6979a3b12c383'
